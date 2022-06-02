dee89e0468126b1a198eb32159e1d80101f6fbf8
Cas van Rijn <c.vanrijn@student.tuelft.nl>
2022-06-02 15:50:42 +0200

    wrote TODO

--- Util/Graph.py
+++ Util/Graph.py
@@ -79,6 +79,9 @@ class Graph:
 
         return False
 
+    def martins_algorithm(self):
+        #TODO write martins algorithm for a graph, so it can be generalized
+
     def show_graph(self, name = 'fsm.gv'):
         f = graphviz.Digraph('finite_state_machine', filename=name)
         f.attr(rankdir='LR', size='8,5')
